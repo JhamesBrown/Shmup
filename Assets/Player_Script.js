fdbad0325871e93aec90f5f8ed0c980c12009870
Luke <luke.stanford@ikegps.com>
2014-05-20 20:50:19 +1200

    added player script from bullethell unity mockup

---
+++ Assets/Player_Script.js
@@ -0,0 +1,154 @@
+﻿#pragma strict
+
+var speed : int = 8;
+
+//dash
+var dash : boolean = false;
+var dashDuration : float = 0.05;
+var dashModifier : int = 10;
+var dashCoolDown : float = 2;
+
+//shooting
+var shot_pref : Transform;
+var fireRate : float = 0.8;
+@HideInInspector var nextShot : float = 0.0;
+
+// aiming pointer
+var aimPointer_pref : Transform;
+var firingAngle : float;
+@HideInInspector var rotation = Quaternion.identity;
+@HideInInspector var pointer : GameObject;
+
+//screenShake effects
+var shake : boolean = false;
+var shakeDuration : float = 0.1; //duration of hit effect
+@HideInInspector var shakeTime : float;
+
+var PlayerGib_pref : Transform;
+
+
+function Start () {
+	
+	// make an aiming pointer on spawn
+	Instantiate(aimPointer_pref, Vector3(transform.position.x, transform.position.y,0), Quaternion.identity);
+	
+}
+
+function Update () {
+	
+
+
+//player movement and playable area limits
+	if(Input.GetAxis("Vertical") != 0){
+		if (transform.position.y > 8.5){
+			transform.position.y = 8.5;
+		}
+		if (transform.position.y < -8.5){
+			transform.position.y = -8.5;
+		}
+		transform.Translate(Vector3(0,(Input.GetAxis("Vertical")) * speed * Time.deltaTime,0));
+		
+	}
+	
+	if(Input.GetAxis("Horizontal") != 0){
+		if (transform.position.x > 7.5){
+			transform.position.x = 7.5;
+		}
+		if (transform.position.x < -7.5){
+			transform.position.x = -7.5;
+		}
+		transform.Translate(Vector3((Input.GetAxis("Horizontal")) * speed * Time.deltaTime,0,0));
+		
+	}
+	
+// dash rewrite
+	if (dash == false){
+		if (Input.GetAxis("Dash")){
+			dash = true;
+			dashFunction();
+			}
+			renderer.material.color = Color(1,1,1);
+	}
+
+//shooting		
+	if (Input.GetAxis("Fire1")){
+		if (Time.time >= nextShot){
+			Instantiate(shot_pref, Vector3(transform.position.x, transform.position.y,0), rotation) ;
+	
+			nextShot = Time.time + fireRate;
+			shakeTime = Time.time;
+			shake = true;
+		}
+		
+	}
+	
+
+	
+	
+//this section controls the firing angle
+	if (Input.GetAxis("rightX") != 0){
+		if (Input.GetAxis("rightX") > 0.05) {
+			firingAngle = 270;
+			
+				if (Input.GetAxis("rightY") != 0) {
+				firingAngle -= 90 * Input.GetAxis("rightY");
+				}
+			
+		}
+			
+		if (Input.GetAxis("rightX") < -0.05) {
+			firingAngle = 90;
+		
+				if (Input.GetAxis("rightY") != 0) {
+				firingAngle += 90 * Input.GetAxis("rightY");
+				}
+		}
+		rotation.eulerAngles = Vector3(0, 0, firingAngle);
+		
+	}
+			
+//screen shake		
+	if (shake && Time.time < shakeTime + shakeDuration){
+		Camera.main.transform.Translate(Vector3(Random.Range(-0.05,0.05),Random.Range(-0.05,0.05),0));
+	}
+	else
+	{
+		shake = false;
+		Camera.main.transform.position = (Vector3(transform.position.x / 3, transform.position.y / 3, -10));
+	}
+	
+		
+}
+
+function OnCollisionEnter (col : Collision){
+
+	if(col.gameObject.tag =="Enemy"){
+			
+			Destroy(col.gameObject);
+			Destroy(gameObject);
+			
+			pointer = GameObject.FindGameObjectWithTag("Aiming_Pointer");
+			Destroy(pointer.gameObject);
+			for (var i = 0; i < 200; i++){
+				Instantiate(PlayerGib_pref, Vector3(transform.position.x, transform.position.y,0), Quaternion.identity);
+			}
+	}
+}	
+
+function dashFunction(){
+	speed *= dashModifier;
+	renderer.material.color = Color(1,0.0,0.0);
+	yield WaitForSeconds(dashDuration);
+	speed /= dashModifier;
+	yield WaitForSeconds (dashCoolDown);
+	dash = false;
+}
+
+
+
+	
+
+
+
+
+
