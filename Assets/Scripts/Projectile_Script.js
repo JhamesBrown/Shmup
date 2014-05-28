af2dbb4edceaf3779ad28246c00868a946f3e253
JhamesBrown <jamestron@gmail.com>
2014-05-28 13:23:18 +1200

    fixed projectiles colliding with themselves
    - projectile colliders turn into triggers onHit meaning they wont
    interfere with trailing projectiles
    
    - adding another simple Drawn projectile to test animator features.

--- Assets/Scripts/Projectile_Script.js
+++ Assets/Scripts/Projectile_Script.js
@@ -6,7 +6,7 @@ var anim : Animator;
 function Start () {
 	shotForce = 350;
 	
-	anim = GetComponent("Animator");
+	anim = GetComponent(Animator);
 	rigidbody2D.AddForce(Vector3.up * shotForce);
 }
 
@@ -15,6 +15,7 @@ function Update () {
 }
 
 function OnCollisionEnter2D (col : Collision2D) {
+	collider2D.isTrigger = true;
 	anim.SetInteger("hit", 1);
 	yield WaitForSeconds(0.3);
 	Destroy(gameObject);
