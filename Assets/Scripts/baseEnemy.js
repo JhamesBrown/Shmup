a202b46fc03968b73dc08e079463b40145bced45
JhamesBrown <jamestron@gmail.com>
2014-06-07 17:19:55 +1200

    added gameManager
    added gameManager
    added basic Enemy spawning for testing
    added pause and exit

--- Assets/Scripts/baseEnemy.js
+++ Assets/Scripts/baseEnemy.js
@@ -4,24 +4,32 @@ var health : int;
 
 function Start () {
 	anim = GetComponent(Animator);
-	health = 100;
+	health = 20;
 }
 
 function Update () {
-	if (health <= 0) {
+	if (health <= 0 && collider2D != null) {
 	collider2D.isTrigger = true;
 		onDeath();
 	}
 }
 function OnCollisionEnter2D (col : Collision2D) {
-	anim.SetInteger("state", 1);
-	health -= 20;
-	yield WaitForSeconds(0.3);
-	anim.SetInteger("state", 0);
+	
+	if(col.gameObject.tag =="Projectile") {
+		anim.SetInteger("state", 1);
+		health -= 10;
+		yield WaitForSeconds(0.3);
+		anim.SetInteger("state", 0);
+		return;
+	}
+	
+	if(col.gameObject.tag =="Wall" && col != null) {
+		health = 0;
+	}
 }
 
 function onDeath() {
 	anim.SetInteger("state", 2);
-	yield WaitForSeconds(0.167);
+	yield WaitForSeconds(0.5);
 	Destroy(gameObject);
 }
---
+++ Assets/Scripts/gameManager_Script.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+var testEnemy : GameObject;
+var nextSpawn : int;
+var spawnRate : int;
+var gameTime : int;
+var paused : boolean;
+var	Enemy_test : Transform; 
+
+function Start () {
+	paused = false;
+	spawnRate = 1;
+}
+
+function Update () {
+
+	if (Input.GetKey ("escape")) {
+			Application.Quit();
+	}
+	gameTime = Time.time;
+	
+//spawning	
+	if (Time.time >= nextSpawn){
+		spawn("test");
+		
+		nextSpawn = Time.time + spawnRate;
+	}
+}
+
+function pause (){
+//pause & unpause
+	if(paused == false){
+		Time.timeScale = 0;
+		paused = true;
+	}
+	else
+	{
+		Time.timeScale = 1;
+		paused = false;
+	
+	}
+}
+function OnGUI(){
+	
+	GUI.Box(Rect(Screen.width - 100, 50, 120,50),""+ gameTime);
+	
+}
+
+//enemy spawn functions
+function spawn(enemy : String){
+
+	if (enemy == "test"){
+		Instantiate(Enemy_test, Vector2(Random.Range(-8,8),7), Quaternion.identity);
+	}
+	
+}
