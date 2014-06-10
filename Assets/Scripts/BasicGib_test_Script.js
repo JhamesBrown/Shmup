fc012c4c1516ca6d8a094f9d30140c4f5d059bb2
JhamesBrown <jamestron@gmail.com>
2014-06-10 21:09:59 +1200

    swapped basicEnemy death
    added gibs with trails to the enemy deaths
    
    **perhaps we should use a particle system for the explosion though.

---
+++ Assets/Scripts/BasicGib_test_Script.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+function Start () {
+	
+	collider2D.isTrigger = false;
+	rigidbody2D.AddForce(Vector3.right * 100 * (Mathf.RoundToInt(Random.Range(-1.0,1.0))));
+	rigidbody2D.AddForce(Vector3.up * 100 * (Mathf.RoundToInt(Random.Range(-1.0,1.0))));
+	if (rigidbody2D.velocity.x == 0 && rigidbody2D.velocity.y == 0){
+		rigidbody2D.AddForce(Vector3.right * 100 * (Mathf.RoundToInt(Random.Range(-1.0,1.0))));
+		rigidbody2D.AddForce(Vector3.up * 100 * (Mathf.RoundToInt(Random.Range(-1.0,1.0))));
+	}
+	countDown();
+}
+
+function Update () {
+	if (rigidbody2D.velocity.x == 0 && rigidbody2D.velocity.y == 0){
+		rigidbody2D.AddForce(Vector3.right * 100 * (Mathf.RoundToInt(Random.Range(-1.0,1.0))));
+		rigidbody2D.AddForce(Vector3.up * 100 * (Mathf.RoundToInt(Random.Range(-1.0,1.0))));
+	}
+}
+
+function countDown() {
+	yield WaitForSeconds (0.3);
+	collider2D.isTrigger = false;
+	yield WaitForSeconds (Random.Range(1.0, 5.0));
+	Destroy(gameObject);
+}
---
+++ Assets/Scripts/baseEnemy_test.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+var health : int;
+var testGib_pref : Transform;
+
+function Start () {
+	health = 20;
+}
+
+function Update () {
+	if (health <= 0 && collider2D != null) {
+		collider2D.isTrigger = true;
+		onDeath();
+	}
+}
+function OnCollisionEnter2D (col : Collision2D) {
+	
+	if(col.gameObject.tag =="Projectile") {
+		health -= 10;
+	}
+	
+	if(col.gameObject.tag =="Wall" && col != null) {
+		health = 0;
+	}
+}
+
+function onDeath() {
+	for (var i = 0; i < 15; i++){
+			// Instantiate(testGib_pref, Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+			Instantiate(testGib_pref, Vector2(transform.position.x + Random.Range(-0.1, 0.1) , transform.position.y + Random.Range(-0.1, 0.1)), Quaternion.identity);
+	}
+	Destroy(gameObject);
+}
