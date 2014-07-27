3d84eaad7647046dd750b9df1afcc2b4b62af7d2
JhamesBrown <jamestron@gmail.com>
2014-07-27 19:10:21 +1200

    added 'wobble' enemy, still needs editing of prefab and behaviour.
    also made some minor clean ups changes to file structure.

---
+++ Assets/Scripts/Enemies/wobble_Script.js
@@ -0,0 +1,62 @@
+﻿#pragma strict
+var health : int;
+var testGib_pref : Transform;
+var baseEnemy_Explosion : GameObject;
+@HideInInspector var gameManager : gameManager_Script;
+var Velocity : Vector2;
+var fallingVelocityMax : float;
+
+
+function Start () {
+	gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent(gameManager_Script);
+	health = 20;
+	fallingVelocityMax = 0.8;
+}
+
+function Update () {
+
+	Velocity = rigidbody2D.velocity;
+	
+	if (health <= 0 && collider2D != null) {
+		collider2D.isTrigger = true;
+		onDeath();
+	}
+	
+	if (rigidbody2D.velocity.y > - fallingVelocityMax){
+		rigidbody2D.AddForce(Vector2.up * (-1));
+	}
+}
+
+function OnCollisionEnter2D (col : Collision2D) {
+
+	if(col.gameObject.tag =="Projectile") {
+		health -= 10;
+	}
+
+	if(col.gameObject.tag =="Wall" && col != null) {
+		Destroy(gameObject);
+	}
+	if(col.gameObject.tag =="Player" && col != null) {
+		health = 0;
+	}
+}
+
+function onDeath() {
+
+	Instantiate (baseEnemy_Explosion,Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+	for (var i = 0; i < 15; i++){
+			Instantiate(testGib_pref, Vector2( AddNoiseToXPosition(), AddNoiseToYPosition()), Quaternion.identity);
+	}
+	gameManager.enemiesKilled ++;
+	Destroy(gameObject);
+}
+
+function AddNoiseToXPosition(){
+  return transform.position.x + Random.Range(-0.1, 0.1);
+
+}
+
+function AddNoiseToYPosition(){
+  return transform.position.y + Random.Range(-0.1, 0.1);
+
+}
--- Assets/Scripts/spawnManager_EnemyTester.js
+++ Assets/Scripts/spawnManager_EnemyTester.js
@@ -7,6 +7,7 @@ var amountinWave : int;
 var Enemy_test : GameObject;
 var upSquirt : GameObject;
 var chaserParent : GameObject;
+var wobble : GameObject;
 var healthPickUp : GameObject;
 static var left : int = -1;
 static var right : int = 1;
@@ -26,11 +27,7 @@ function Update () {
 function spawnTimer(){
 	
 	yield WaitForSeconds (1);
-	spawn("chaser", Vector2(1,-7));
-	yield WaitForSeconds (1);
-	spawn("chaser", Vector2(2,-7));
-	yield WaitForSeconds (1);
-	spawn("chaser", Vector2(3,-7));
+	diaglineSpawn(-7,right, 10, 1);
 	
 	
 	
@@ -57,11 +54,14 @@ function spawn(enemy : String, spawnPosition : Vector2){
 	if (enemy == "healthPickUp"){
 		Instantiate(healthPickUp, spawnPosition, Quaternion.identity);
 	}
+	if (enemy == "wobble"){
+		Instantiate(wobble, spawnPosition, Quaternion.identity);
+	}
 }
 
 function diaglineSpawn (startPoint : int, direction : int, amountinWave : int, spawnInterval : float) {
 	for (var i : int = 0; i < amountinWave && startPoint + (i*direction) < 9 && startPoint + (i*direction) > -9 ; i++) {
-				Instantiate(Enemy_test, Vector2(startPoint + (i*direction),7), Quaternion.identity);
+				Instantiate(wobble, Vector2(startPoint + (i*direction),7), Quaternion.identity);
 				yield WaitForSeconds (spawnInterval);
 	}
 }
